159823d0acc953ae1e4514cf64eb82a3f75bb882
Harry (OpenERP) <hmo@tinyerp.com>
2011-11-09 23:19:39 +0530

    [ADD] project_merge_proposal: * take merge proposal details from launchpad and create/modify work details into openerp * take comments on merge propodal from launchpad and create/modify review details into openerp

---
+++ project_merge_proposal/project_launchpad_scheduler.py
@@ -0,0 +1,231 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from osv import osv
+
+from launchpadlib.launchpad import Launchpad, EDGE_SERVICE_ROOT
+from datetime import datetime, date
+from bzrlib.branch import Branch
+import os
+
+MERGE_STATUS = {'Needs review': 'needs_review', 'Code failed to merge':'code_failed', 'Work in progress':'in_reviewing', 'Approved': 'ready', 'Rejected':'rejected' } # 'Queued': 'pending', 'Superseded':'cancelled', 'Merged':'done' }
+
+BRANCH_STATUS = {'Experimental':'experimental', 'Development': 'development', 'Mature':'mature'} #'Merged':'merged', 'Abandoned':'cancelled'}
+COMMENT_STATUS = { 'Resubmit':'resubmit',
+                   'Approve': 'approved',
+                   'Needs Fixing': 'need_fixing',
+                   'Needs Information': 'need_information',
+                   'Disapprove': 'disapproved' }
+DEFAULT_LOGIN = 'hmo-tinyerp'
+DEFAULT_CACHE_PATH = "~/.launchpadlib/cache"
+
+TEAMS = ['openerp-dev'] #,'openobject-training', 'openerp-opw']
+PROJECTS = ['openerp', 'openobject-addons', 'openobject-client', 'openerp-web']
+
+LP_API_LINK = "https://api.edge.launchpad.net/1.0/"
+def get_lp():
+    cachedir = os.path.expanduser(DEFAULT_CACHE_PATH)
+    return Launchpad.login_with(DEFAULT_LOGIN, EDGE_SERVICE_ROOT, cachedir)
+
+class project_launchpad_scheduler(osv.osv_memory):
+    _name = "project.launchpad.scheduler"
+    launchpad = None
+
+    def get_email_address(self, people):
+        return [email_address.email for email_address in people.confirmed_email_addresses]
+
+    def load_link(self, link):
+        return self.launchpad.load(link)
+
+    def get_lp_people(self, people):
+        if self.launchpad is None:
+            self.launchpad = get_lp()
+        return self.launchpad.people[people]
+
+    def get_lp_project(self, project):
+        if project is None or not project or project == 'none':
+            return False
+        if self.launchpad is None:
+            self.launchpad = get_lp()
+        return self.launchpad.projects[project]
+
+    def get_lp_people_by_email(self, email):
+        if self.launchpad is None:
+            self.launchpad = get_lp()
+        return self.launchpad.people.getByEmail(email=email)
+
+    def get_lp_branches(self, people):
+        return people.getBranches(status=BRANCH_STATUS.keys())
+
+
+    def get_lp_merge_proposal(self, branch, from_date=False):
+        merge_requests = []
+        for proposal in branch.getMergeProposals(status=MERGE_STATUS.keys()):
+            proposal_date = proposal.date_created
+            branch_date = branch.date_created
+            if from_date and datetime(proposal_date.year, proposal_date.month, proposal_date.day) <= datetime(from_date.year, from_date.month, from_date.day):
+                continue
+            merge_request = {}
+            merge_request['date_created'] = proposal_date
+            merge_request['date_created_branch'] = branch_date
+            preview_diff = proposal.preview_diff #_link and self.load_link(proposal.preview_diff_link) or None
+
+            merge_request['added_lines_count'] = preview_diff and preview_diff.added_lines_count or 0
+            merge_request['remove_lines_count'] = preview_diff and preview_diff.removed_lines_count or 0
+            merge_request['diff_lines_count'] = preview_diff and preview_diff.diff_lines_count or 0
+            merge_request['diffstat'] = preview_diff and (preview_diff.diffstat or {}) or {}
+            merge_request['diffstat_files_count'] = len(merge_request['diffstat'].keys())
+            merge_request['description'] = proposal.description
+            merge_request['state'] = MERGE_STATUS.get(proposal.queue_status)
+            merge_request['name'] = proposal.self_link.replace(LP_API_LINK, "")
+            merge_request['source_branch_link'] = proposal.source_branch_link.replace(LP_API_LINK, "")
+            merge_request['target_branch_link'] = proposal.target_branch_link.replace(LP_API_LINK, "")
+            merge_request['registrant_name'] = proposal.registrant.name
+            merge_request['registrant_email_address'] = self.get_email_address(proposal.registrant)
+
+            comments = []
+            for review in proposal.all_comments_collection:
+                comment = {}
+                comment['date'] = review.date_created
+                comment['author_name'] = review.author.name
+                comment['author_email'] = self.get_email_address(review.author)
+                comment['message'] = review.message_body
+                comment['title'] = review.title
+                comment['id'] = review.id
+                comment['vote'] = COMMENT_STATUS.get(review.vote)
+                comment['branch'] = proposal.source_branch_link
+                comments.append(comment)
+            merge_request['comments'] = comments
+            merge_requests.append(merge_request)
+
+        return merge_requests
+
+    def get_revisions(self, branch_name, location, from_date=False):
+        branch = Branch.open_containing(location)[0]
+
+        #last_revision = branch.revno() - 100
+        revisions = branch.revision_history()
+        commits = []
+        for revision in revisions:
+            rev_id = branch.repository.get_revision(revision)
+            commit_time = rev_id.timestamp
+            commit_date = date.fromtimestamp(commit_time)
+            if from_date and datetime(commit_date.year, commit_date.month, commit_date.day) <= datetime(from_date.year, from_date.month, from_date.day):
+                continue
+            commit = {}
+            commit['date'] = commit_date
+            rev_no = branch.revision_id_to_revno(rev_id)
+            commit['rev_id'] = rev_id
+            commit['rev_no'] = rev_no
+            commit['message'] = revision.get_summary().encode('utf-8')
+            commit['author_email'] = revision.get_apparent_author().encode('utf-8')
+            commit['ass_bugs'] =[[rev_no,bug] for bug in revision.iter_bugs()]
+            commit['branch'] = branch_name
+            commits.append(commit)
+        return commits
+
+    def get_user_by_launchpad_login(self, cr, uid, launchpad_login):
+        res_users = self.pool.get('res.users')
+        login = launchpad_login.split('-')
+        user_ids = res_users.search(cr, uid, [('login','=',login[0])])
+        if user_ids:
+            return user_ids[0]
+        return False
+
+    def _create_work_review(self, cr, uid, work_id, commits, context=None):
+        task_work_review = self.pool.get('project.task.work.review')
+        new_ids = []
+        for commit in commits:
+            user_id = self.get_user_by_launchpad_login(cr, uid, commit['author_name'])
+            vals = {
+                    'ref': commit['id'],
+                    'name': commit['title'],
+                    'description': commit['message'],
+                    'date': commit['date'],
+                    'state': commit.get('vote', 'none') or 'none',
+                    'user_id': user_id,
+                    'user_email': commit['author_name'],
+                    'work_id': work_id,
+            }
+            ids = task_work_review.search(cr, uid, [('ref','=', commit['id'])])
+            if ids and len(ids):
+                task_work_review.write(cr, uid, ids, vals, context=context)
+                new_id = ids[0]
+            else:
+                new_id = task_work_review.create(cr, uid, vals, context=context)
+            #print '>>>>>> line:', new_id, user_id, commit['author_name'], commit['title'], commit.get('vote', 'none') or 'none'
+            new_ids.append(new_id)
+        return new_ids
+
+    def _create_work(self, cr, uid, merge_proposals,context=None):
+        task_work = self.pool.get('project.task.work')
+        new_ids = []
+        for merge_proposal in merge_proposals:
+            user_id = self.get_user_by_launchpad_login(cr, uid, merge_proposal['registrant_name'])
+            vals = {
+                     'name':merge_proposal['name'],
+                     'date':merge_proposal['date_created'],
+                     'date_started': merge_proposal['date_created_branch'],
+                     'user_id':user_id,
+                     'user_name': merge_proposal['registrant_name'],
+                     'description':merge_proposal['description'],
+                     'state':merge_proposal['state'],
+                     'source_branch_link': merge_proposal['source_branch_link'],
+                     'target_branch_link': merge_proposal['target_branch_link'],
+                     'diff_new_lines':merge_proposal['added_lines_count'],
+                     'diff_rem_lines':merge_proposal['remove_lines_count'],
+                     'diff_modification_files':merge_proposal['diffstat_files_count'],
+                     'diff_modification_lines':merge_proposal['diff_lines_count']
+            }
+            ids = task_work.search(cr, uid, [('name','like', merge_proposal['name'])])
+            if ids and len(ids):
+                task_work.write(cr, uid, ids, vals, context=context)
+                new_id = ids[0]
+            else:
+                new_id = task_work.create(cr, uid, vals, context=context)
+            # Take all comments of merge  proposal
+            #print '>>>>>> work:', new_id, user_id, merge_proposal['registrant_name'], merge_proposal['name'], merge_proposal['state']
+            self._create_work_review(cr, uid, new_id, merge_proposal['comments'], context=context)
+            new_ids.append(new_id)
+        return new_ids
+
+    def process_merge_proposals(self, cr, uid, ids=None, context=None):
+        #print 'started process...'
+        for team in TEAMS:
+            lp_people = self.get_lp_people(team)
+            lp_branches = self.get_lp_branches(lp_people)
+            for lp_branch in lp_branches:
+                lp_project = self.get_lp_project(lp_branch.project_link)
+                if not lp_project:
+                    continue
+                if lp_project.name not in PROJECTS:
+                    continue
+                # take all commit from lp for the employee
+                #commits = project_lp.get_revisions(lp_branch.name, lp_branch.web_link, from_date=FROM_DATE)
+                #self._create_timesheet_line(cr, uid, commits, context=context)
+
+                #take all  merge proposal from lp which is sent by contributors
+                merge_proposals = self.get_lp_merge_proposal(lp_branch)
+                self._create_work(cr, uid, merge_proposals, context=context)
+                cr.commit()
+        return True
+
+project_launchpad_scheduler()
---
+++ project_merge_proposal/project_merge_proposal.py
@@ -0,0 +1,78 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from osv import osv, fields
+import datetime
+
+WORK_STATE = [('draft', 'New'), ('open', 'In Progress'), ('needs_review', 'Needs review'), ('code_failed', 'Code failed to merge'), ('pending', 'Pending'),('in_reviewing','Review in progress'), ('ready', 'Approved'), ('done', 'Done'), ('cancelled', 'Cancelled'), ('rejected', 'Rejected')]
+
+REVIEW_STATE = [('resubmit', 'Resubmit'), ('none','No Comments'),
+                   ('approved', 'Approve'),
+                   ('need_fixing', 'Needs Fixing'),
+                   ('need_information', 'Needs Information'),
+                   ('disapproved', 'Disapprove')
+             ]
+
+class project_work_review(osv.osv):
+    _name = "project.task.work.review"
+    _columns = {
+        'ref': fields.char("References Number", size=128),
+        'name': fields.char('Review summary', size=128),
+        'date': fields.datetime('Date', select="1"),
+        'work_id': fields.many2one('project.task.work', 'Work', ondelete='cascade', required=True, select="1"),
+        'hours': fields.float('Time Spent'),
+        'user_id': fields.many2one('res.users', 'Review by'),
+        'company_id': fields.related('work_id', 'company_id', type='many2one', relation='res.company', string='Company', store=True, readonly=True),
+        'user_name': fields.char('User', size=128),
+        'description': fields.text('Description'),
+        'state': fields.selection(REVIEW_STATE, 'State', required=True),
+    }
+    _defaults = {
+        'state': 'none',
+        'user_id': lambda obj, cr, uid, context: uid,
+        'company_id': lambda self, cr, uid, c: self.pool.get('res.company')._company_default_get(cr, uid, 'project.task', context=c)
+    }
+project_work_review()
+
+class project_merge_proposal(osv.osv):
+    _inherit = "project.task.work"
+    _columns = {
+                'review_ids': fields.one2many('project.task.work.review', 'work_id', 'Reviews'),
+                'task_id': fields.many2one('project.task', 'Task'),
+                'date_started': fields.datetime('Started Date'),
+                'date': fields.datetime('Submitted Date'),
+                'source_branch_link': fields.char('Source Branch', size=512),
+                'target_branch_link': fields.char('Target Branch', size=512),
+                'state': fields.selection(WORK_STATE, 'State', required=True),
+                'description': fields.text('Description'),
+                'user_name': fields.char('User Name',size=128),
+                'user_id': fields.many2one('res.users', 'Submitted by'),
+                'diff_new_lines': fields.integer('Diff. New Lines'),
+                'diff_rem_lines': fields.integer('Diff. Remove Lines'),
+                'diff_modification_files': fields.integer('Diff. Modification Files'),
+                'diff_modification_lines': fields.integer('Diff. Modification Lines'),
+    }
+    _defaults = {
+        'state': 'draft',
+    }
+
+project_merge_proposal()
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

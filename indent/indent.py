e9383e45c815e965d765f9f5fe2ce1c75938653e
DJ Patel <mdi@tinyerp.com>
2013-02-21 22:28:15 +0530

    [FIX] indent : Fixed the procurement workflow.

--- indent/indent.py
+++ indent/indent.py
@@ -26,6 +26,7 @@ import openerp.addons.decimal_precision as dp
 from openerp.tools.translate import _
 from openerp.tools import amount_to_text_en as text
 from openerp import netsvc
+from openerp.tools import DEFAULT_SERVER_DATE_FORMAT, DEFAULT_SERVER_DATETIME_FORMAT
 
 class indent_indent(osv.Model):
     _name = 'indent.indent'
@@ -545,5 +546,97 @@ class purchase_requisition(osv.Model):
         return res
 
 purchase_requisition()
+
+class procurement_order(osv.osv):
+    _inherit = 'procurement.order'
+
+    def make_po(self, cr, uid, ids, context=None):
+        """ Make purchase order from procurement
+        @return: New created Purchase Orders procurement wise
+        """
+        res = {}
+        if context is None:
+            context = {}
+        company = self.pool.get('res.users').browse(cr, uid, uid, context=context).company_id
+        uom_obj = self.pool.get('product.uom')
+        pricelist_obj = self.pool.get('product.pricelist')
+        prod_obj = self.pool.get('product.product')
+        acc_pos_obj = self.pool.get('account.fiscal.position')
+        seq_obj = self.pool.get('ir.sequence')
+        warehouse_obj = self.pool.get('stock.warehouse')
+        requisition_obj = self.pool.get('purchase.requisition')
+        for procurement in self.browse(cr, uid, ids, context=context):
+            res_id = procurement.move_id.id
+            partner = procurement.product_id.seller_id # Taken Main Supplier of Product of Procurement.
+            seller_qty = procurement.product_id.seller_qty
+            partner_id = partner.id
+            pricelist_id = partner.property_product_pricelist_purchase.id
+            warehouse_id = warehouse_obj.search(cr, uid, [('company_id', '=', procurement.company_id.id or company.id)], context=context)
+            uom_id = procurement.product_id.uom_po_id.id
+
+            qty = uom_obj._compute_qty(cr, uid, procurement.product_uom.id, procurement.product_qty, uom_id)
+            if seller_qty:
+                qty = max(qty,seller_qty)
+
+            price = pricelist_obj.price_get(cr, uid, [pricelist_id], procurement.product_id.id, qty, partner_id, {'uom': uom_id})[pricelist_id]
+
+            schedule_date = self._get_purchase_schedule_date(cr, uid, procurement, company, context=context)
+            purchase_date = self._get_purchase_order_date(cr, uid, procurement, company, schedule_date, context=context)
+
+            #Passing partner_id to context for purchase order line integrity of Line name
+            new_context = context.copy()
+            new_context.update({'lang': partner.lang, 'partner_id': partner_id})
+
+            product = prod_obj.browse(cr, uid, procurement.product_id.id, context=new_context)
+            taxes_ids = procurement.product_id.supplier_taxes_id
+            taxes = acc_pos_obj.map_tax(cr, uid, partner.property_account_position, taxes_ids)
+
+            name = product.partner_ref
+            if product.description_purchase:
+                name += '\n'+ product.description_purchase
+            line_vals = {
+                'name': name,
+                'product_qty': qty,
+                'product_id': procurement.product_id.id,
+                'product_uom': uom_id,
+                'price_unit': price or 0.0,
+                'date_planned': schedule_date.strftime(DEFAULT_SERVER_DATETIME_FORMAT),
+                'move_dest_id': res_id,
+                'taxes_id': [(6,0,taxes)],
+            }
+            name = seq_obj.get(cr, uid, 'purchase.order') or _('PO: %s') % procurement.name
+            po_vals = {
+                'name': name,
+                'origin': procurement.origin,
+                'partner_id': partner_id,
+                'location_id': procurement.location_id.id,
+                'warehouse_id': warehouse_id and warehouse_id[0] or False,
+                'pricelist_id': pricelist_id,
+                'date_order': purchase_date.strftime(DEFAULT_SERVER_DATETIME_FORMAT),
+                'company_id': procurement.company_id.id,
+                'fiscal_position': partner.property_account_position and partner.property_account_position.id or False,
+                'payment_term_id': partner.property_supplier_payment_term.id or False,
+            }
+            res[procurement.id] = self.create_procurement_purchase_order(cr, uid, procurement, po_vals, line_vals, context=new_context)
+            requisition_id = False
+            if procurement.product_id.purchase_requisition:
+                requisition_id = requisition_obj.create(cr, uid, 
+                    {
+                        'origin': procurement.origin,
+                        'date_end': procurement.date_planned,
+                        'warehouse_id':warehouse_id and warehouse_id[0] or False,
+                        'company_id':procurement.company_id.id,
+                        'line_ids': [(0,0,{
+                            'product_id': procurement.product_id.id,
+                            'product_uom_id': procurement.product_uom.id,
+                            'product_qty': procurement.product_qty
+                        })],
+                    })
+                requisition_obj.write(cr, uid, [requisition_id], {'purchase_ids': [(6, 0, res.values())]}, context=context)
+            self.write(cr, uid, [procurement.id], {'state': 'running', 'purchase_id': res[procurement.id], 'requisition_id': requisition_id})
+        self.message_post(cr, uid, ids, body=_("Draft Purchase Order created"), context=context)
+        return res
+
+procurement_order()
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

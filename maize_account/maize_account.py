e3d66b829cfb0135ea0c12ddecab9d1ebe3d6bd6
DJ Patel <mdi@tinyerp.com>
2013-03-14 19:10:36 +0530

    [FIX] maize_account : Fix the tax computation when included in base amount.

--- maize_account/maize_account.py
+++ maize_account/maize_account.py
@@ -39,6 +39,83 @@ class account_tax(osv.Model):
     _defaults = {
         'tax_type': 'other',
     }
+
+    def _unit_compute(self, cr, uid, taxes, price_unit, product=None, partner=None, quantity=0):
+        taxes = self._applicable(cr, uid, taxes, price_unit , product, partner)
+        res = []
+        cur_price_unit = price_unit
+        for tax in taxes:
+            # we compute the amount for the current tax object and append it to the result
+            data = {'id':tax.id,
+                    'name':tax.description and tax.description + " - " + tax.name or tax.name,
+                    'account_collected_id':tax.account_collected_id.id,
+                    'account_paid_id':tax.account_paid_id.id,
+                    'account_analytic_collected_id': tax.account_analytic_collected_id.id,
+                    'account_analytic_paid_id': tax.account_analytic_paid_id.id,
+                    'base_code_id': tax.base_code_id.id,
+                    'ref_base_code_id': tax.ref_base_code_id.id,
+                    'sequence': tax.sequence,
+                    'base_sign': tax.base_sign,
+                    'tax_sign': tax.tax_sign,
+                    'ref_base_sign': tax.ref_base_sign,
+                    'ref_tax_sign': tax.ref_tax_sign,
+                    'price_unit': cur_price_unit,
+                    'tax_code_id': tax.tax_code_id.id,
+                    'ref_tax_code_id': tax.ref_tax_code_id.id,
+                    'include_base_amount': tax.include_base_amount,
+                    'parent_id':tax.parent_id
+            }
+            res.append(data)
+            if tax.type == 'percent':
+                amount = cur_price_unit * tax.amount
+                data['amount'] = amount
+
+            elif tax.type == 'fixed':
+                data['amount'] = tax.amount
+                data['tax_amount'] = quantity
+                # data['amount'] = quantity
+            elif tax.type == 'code':
+                localdict = {'price_unit':cur_price_unit, 'product':product, 'partner':partner}
+                exec tax.python_compute in localdict
+                amount = localdict['result']
+                data['amount'] = amount
+            elif tax.type == 'balance':
+                data['amount'] = cur_price_unit - reduce(lambda x, y: y.get('amount', 0.0) + x, res, 0.0)
+                data['balance'] = cur_price_unit
+
+            amount2 = data.get('amount', 0.0)
+            if tax.child_ids:
+                if tax.child_depend:
+                    latest = res.pop()
+                amount = amount2
+                child_tax = self._unit_compute(cr, uid, tax.child_ids, amount, product, partner, quantity)
+                # Add Parent reference in child dictionary of tax so that we can inlcude tha amount of child ...
+                for ctax in child_tax:
+                    ctax['parent_tax'] = tax.id
+                
+                res.extend(child_tax)
+                if tax.child_depend:
+                    for r in res:
+                        for name in ('base', 'ref_base'):
+                            if latest[name + '_code_id'] and latest[name + '_sign'] and not r[name + '_code_id']:
+                                r[name + '_code_id'] = latest[name + '_code_id']
+                                r[name + '_sign'] = latest[name + '_sign']
+                                r['price_unit'] = latest['price_unit']
+                                latest[name + '_code_id'] = False
+                        for name in ('tax', 'ref_tax'):
+                            if latest[name + '_code_id'] and latest[name + '_sign'] and not r[name + '_code_id']:
+                                r[name + '_code_id'] = latest[name + '_code_id']
+                                r[name + '_sign'] = latest[name + '_sign']
+                                r['amount'] = data['amount']
+                                latest[name + '_code_id'] = False
+            if tax.include_base_amount:
+                cur_price_unit += amount2
+                # Check for Child tax addition. If Tax has childrens and they have also set include in base amount we will add it for next tax calculation...
+                for r in res:
+                    if 'parent_tax' in r and r['parent_tax'] == tax.id:
+                        cur_price_unit += r['amount']
+        return res
+
 account_tax()
 
 class account_invoice_tax(osv.osv):

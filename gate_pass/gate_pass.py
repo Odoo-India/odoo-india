34701c14b372b11c348c9540d61906af89cfd1f5
DJ Patel <mdi@tinyerp.com>
2013-03-21 18:21:55 +0530

    [ADD] gate_pass : Added a functionality to automatically create a gate pass when incoming shipment and delivery order is created.

--- gate_pass/gate_pass.py
+++ gate_pass/gate_pass.py
@@ -27,22 +27,47 @@ class stock_picking(osv.Model):
 
     _columns = {
         'series':fields.selection([('repair', 'Repair'), ('purchase', 'Purchase'), ('store', 'Store')], 'Series'),
-        'type':fields.selection([('foc', 'Free Of Cost'), ('chargeable', 'Chargeable'), ('sample', 'Sample'), ('contract', 'Contract'), ('cash', 'Cash'), ('Loan', 'Loan')], 'Type'),
+        'gate_pass_type':fields.selection([('foc', 'Free Of Cost'), ('chargeable', 'Chargeable'), ('sample', 'Sample'), ('contract', 'Contract'), ('cash', 'Cash'), ('Loan', 'Loan')], 'Type'),
+        'gate_pass_id': fields.many2one('gate.pass', 'Gate Pass'),
     }
 
 stock_picking()
 
 class stock_picking_in(osv.Model):
-    _name = "stock.picking.in"
-    _inherit = "stock.picking"
+    _inherit = "stock.picking.in"
     _table = "stock_picking"
+
+    _columns = {
+        'series':fields.selection([('repair', 'Repair'), ('purchase', 'Purchase'), ('store', 'Store')], 'Series'),
+        'gate_pass_type':fields.selection([('foc', 'Free Of Cost'), ('chargeable', 'Chargeable'), ('sample', 'Sample'), ('contract', 'Contract'), ('cash', 'Cash'), ('Loan', 'Loan')], 'Type'),
+        'gate_pass_id': fields.many2one('gate.pass', 'Gate Pass'),
+    }
+
+    def create(self, cr, uid, vals, context=None):
+        gate_pass_obj = self.pool.get('gate.pass')
+        value = dict(partner_id = vals.get('partner_id'))
+        gate_pass_id = gate_pass_obj.create(cr, uid, value, context=context)
+        vals['gate_pass_id'] = gate_pass_id
+        return super(stock_picking_in, self).create(cr, uid, vals, context=context)
 
 stock_picking_in()
 
 class stock_picking_out(osv.Model):
-    _name = "stock.picking.out"
-    _inherit = "stock.picking"
+    _inherit = "stock.picking.out"
     _table = "stock_picking"
+
+    _columns = {
+        'series':fields.selection([('repair', 'Repair'), ('purchase', 'Purchase'), ('store', 'Store')], 'Series'),
+        'gate_pass_type':fields.selection([('foc', 'Free Of Cost'), ('chargeable', 'Chargeable'), ('sample', 'Sample'), ('contract', 'Contract'), ('cash', 'Cash'), ('Loan', 'Loan')], 'Type'),
+        'gate_pass_id': fields.many2one('gate.pass', 'Gate Pass'),
+    }
+
+    def create(self, cr, uid, vals, context=None):
+        gate_pass_obj = self.pool.get('gate.pass')
+        value = dict(partner_id = vals.get('partner_id'))
+        gate_pass_id = gate_pass_obj.create(cr, uid, value, context=context)
+        vals['gate_pass_id'] = gate_pass_id
+        return super(stock_picking_out, self).create(cr, uid, vals, context=context)
 
 stock_picking_out()
 
@@ -51,13 +76,13 @@ class gate_pass(osv.Model):
     _description = 'Gate Pass'
 
     _columns = {
-        'name': fields.char('Name', size=256, required=True),
+        'name': fields.char('Name', size=256),
         'gate_pass_no': fields.char('Gate Pass No', size=256, required=True),
         'picking_id': fields.many2one('stock.picking', 'Picking'),
         'series':fields.related('picking_id', 'series', type='selection', string='Series', store=True),
-        'type':fields.related('picking_id', 'type', type='selection', string='Type', store=True),
+        'type':fields.related('picking_id', 'gate_pass_type', type='selection', string='Type', store=True),
         'date': fields.datetime('Gate Pass Date', required=True),
-        'partner_id':fields.many2one('res.partner', 'Supplier', required=True),
+        'partner_id':fields.many2one('res.partner', 'Supplier'),
         'department_id': fields.many2one('stock.location', 'Department', required=True),
         'indent_id': fields.many2one('indent.indent', 'Indent'),
         'move_lines': fields.one2many('stock.move', 'gate_pass_id', 'Products'),
@@ -82,7 +107,7 @@ class stock_move(osv.Model):
     _inherit = 'stock.move'
 
     _columns = {
-        'gate_pass_id': fields.many2one('gate.pass', 'Gate Pass', required=True, ondelete='cascade'),
+        'gate_pass_id':fields.related('picking_id', 'gate_pass_id', type='many2one', relation='stock.picking', string='Gate Pass', store=True),
     }
 
 stock_move()

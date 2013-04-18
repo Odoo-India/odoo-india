2b1fff7dee0c2816225c87c00e54f83bc0d0eb35
DJ Patel <mdi@tinyerp.com>
2013-04-18 21:10:11 +0530

    [FIX] indent : NoneType object has not attribute _table.

--- indent/indent.py
+++ indent/indent.py
@@ -674,7 +674,6 @@ class stock_picking(osv.Model):
         'lab_no':fields.integer("Lab No"),
         'gp_no': fields.integer("Gate Pass No"),
         'gp_year': fields.char("GP Year",size=64),
-        'series_id': fields.many2one("product.order.series",'Series'),
         'remark1': fields.char("Remark1",size=256),
         'remark2': fields.char("remark2",size=256),
         'case_code': fields.boolean("Cash Code"),

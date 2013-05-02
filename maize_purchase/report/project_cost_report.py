0c500c522519daa89adb0dde592cb60afb38d20d
Kuldeep (Open ERP) <kjo@tinyerp.com>
2013-05-02 15:39:52 +0530

    [IMP] Project cost report change

--- maize_purchase/report/project_cost_report.py
+++ maize_purchase/report/project_cost_report.py
@@ -28,8 +28,6 @@ class project_cost_report(osv.osv):
     _auto = False
 
     _columns = {
-        'purchase_id': fields.many2one('purchase.order', 'PO Number', readonly=True),
-        'indent_id': fields.many2one('indent.indent', 'Indent', readonly=True),
         'indentor_id': fields.many2one('res.users', 'Indentor', readonly=True),
         'project_code': fields.char('Project Code',size=28),
         'project_name': fields.char('Project Name', size=256),
@@ -43,7 +41,6 @@ class project_cost_report(osv.osv):
             ('reject','Rejected')
             ], 'State', readonly=True),
         'analytic_account_id': fields.many2one('account.analytic.account', 'Project', readonly=True),
-        'price_unit': fields.float('Rate', readonly=True),
         'contract': fields.boolean('Contract'),
         'puchase_total': fields.float('PO Value', readonly=True),
         'receipt_total': fields.float('Receipt', readonly=True),
@@ -57,44 +54,28 @@ class project_cost_report(osv.osv):
         cr.execute("""
             create or replace view project_cost_report as (
                 select
-                    min(po.id) as id,
-                    i.id as indent_id,
-                    po.id as purchase_id,
+                    min(i.id) as id,
+                    i.indentor_id as indentor_id,
                     i.contract as contract,
-                    l.price_unit as price_unit,
-                    sum(l.product_qty * l.price_unit) as price_total,
-                    po.partner_id as partner_id,
-                    po.amount_total as puchase_total,
-                    sp.amount_total as receipt_total,
+                    sum(po.amount_total) as puchase_total,
+                    sum(sp.amount_total) as receipt_total,
                     a.name as project_name,
                     a.code as project_code,
                     sum(sp.amount_total - 0) as difference,
                     1 as nbr,
-                    i.indentor_id as indentor_id,
                     i.state,
                     i.analytic_account_id as analytic_account_id
                 from
                     indent_indent i
                     left join purchase_order po on (i.id=po.indent_id)
-                    left join purchase_order_line l on (l.order_id = po.id)
-                    left join product_product p on (l.product_id=p.id)
-                    left join product_template t on (p.product_tmpl_id=t.id)
                     left join stock_picking sp on (po.id = sp.purchase_id)
                     left join account_analytic_account a on (i.analytic_account_id = a.id)
-                where po.indent_id is not null and l.product_id is not null and sp.type in ('out', 'in', 'receipt')
+                where po.indent_id is not null
                 group by
-                    po.id,
-                    i.id,
                     i.contract,
-                    po.amount_total,
                     i.indentor_id,
                     i.state,
                     i.analytic_account_id,
-                    l.product_qty,
-                    l.price_unit,
-                    po.partner_id,
-                    i.analytic_account_id,
-                    sp.amount_total,
                     a.name,
                     a.code
             )

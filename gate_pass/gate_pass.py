c48fc5ebe8cfb499bb8dd094406ee62308f3be01
DJ Patel <mdi@tinyerp.com>
2013-05-03 14:39:19 +0530

    [ADD] gate_pass : Improved an onchange method.

--- gate_pass/gate_pass.py
+++ gate_pass/gate_pass.py
@@ -148,9 +148,9 @@ class stock_picking_out(osv.Model):
             return {'value': result}
         move_ids = []
         products = indent_obj.browse(cr, uid, indent, context=context).product_lines
+        location_id = self.pool.get('ir.model.data').get_object_reference(cr, uid, 'stock', 'stock_location_stock')
+        location = location_id and location_id[1] or False
         for product in products:
-            location_id = self.pool.get('ir.model.data').get_object_reference(cr, uid, 'stock', 'stock_location_stock')
-            location = location_id and location_id[1] or False
             vals = dict(product_id = product.product_id.id, product_qty = product.product_uom_qty, product_uom = product.product_uom.id, name = product.product_id.name, location_id = location, location_dest_id = location)
             move_ids.append(move_obj.create(cr, uid, vals, context=context))
         result['move_lines'] = move_ids

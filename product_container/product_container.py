0a5120c8f4d7469bc1959a0d8e09389c4de1d719
DJ Patel <mdi@tinyerp.com>
2013-06-11 15:07:17 +0530

    [FIX] product_container : Fixed the duplication of product containers.

--- product_container/product_container.py
+++ product_container/product_container.py
@@ -40,7 +40,8 @@ class stock_picking_in(osv.Model):
         for picking in self.browse(cr, uid, ids, context=context):
             if picking.type == 'in':
                 for move in picking.move_lines:
-                    if move.prodlot_id and move.product_id and move.product_id.return_container and move.product_id.product_container_id:
+                    if not move.manage_container and move.prodlot_id and move.product_id and move.product_id.return_container and move.product_id.product_container_id:
+                        move_obj.write(cr, uid, [move.id], {'manage_container': True}, context=context)
                         default = dict(product_id = move.product_id.product_container_id.id)
                         new_prodlot = prodlot_obj.copy(cr, uid, move.prodlot_id.id, default, context=context)
                         default = dict(product_id = move.product_id.product_container_id.id, product_uom = move.product_id.product_container_id.uom_id.id, product_qty = 1, prodlot_id = new_prodlot, state = 'assigned')
@@ -48,5 +49,18 @@ class stock_picking_in(osv.Model):
         return super(stock_picking_in, self).action_process(cr, uid, ids, context=context)
 
 stock_picking_in()
+
+class stock_move(osv.Model):
+    _inherit = 'stock.move'
+
+    _columns = {
+        'manage_container': fields.boolean('Manage Container'),
+    }
+
+    _defaults = {
+        'manage_container': False,
+    }
+
+stock_move()
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

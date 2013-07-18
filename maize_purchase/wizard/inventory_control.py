c26f6de361cb7de8e79ac5740b997ee8b8090c69
Bharat R. Devnani (OpenERP) <bde@tinyerp.com>
2013-07-18 12:19:47 +0530

    [ADD] added warning messages in wizard for product information report

--- maize_purchase/wizard/inventory_control.py
+++ maize_purchase/wizard/inventory_control.py
@@ -20,6 +20,7 @@
 ##############################################################################
 
 from openerp.osv import fields, osv
+from openerp.tools.translate import _
 
 class inventory_control(osv.osv_memory):
     
@@ -57,6 +58,24 @@ class inventory_control(osv.osv_memory):
         date_from = browse_rec.date_from 
         date_to = browse_rec.date_to 
         po_series_id = browse_rec.po_series_id.id
+        
+        if supplier_code_from and not supplier_code_to or supplier_code_to and not supplier_code_from:
+            raise osv.except_osv(_("Warning !"), _("Enter values in both Supplier From and Supplier To."))
+        
+        if item_code_from and not item_code_to or item_code_to and not item_code_from:
+            raise osv.except_osv(_("Warning !"), _("Enter values in both Item Code From and Item Code To."))
+        
+        if date_from and not date_to or date_to and not date_from:
+            raise osv.except_osv(_("Warning !"), _("Enter values in both Date From and Date To."))
+        
+        if str(supplier_code_from) > str(supplier_code_to):
+            raise osv.except_osv(_("Warning !"), _("Supplier To Code should be greater than Supplier From Code."))
+        
+        if str(item_code_from) > str(item_code_to):
+            raise osv.except_osv(_("Warning !"), _("Item Code To should be greater than Item Code From."))
+        
+        if date_from > date_to:
+            raise osv.except_osv(_("Warning !"), _("Date To should be greater than Date From."))
         
         action_model, action_id = tuple(mod_obj.get_object_reference(cr, uid, 'maize_purchase', 'action_purchase_order_info_report'))
         action = self.pool.get(action_model).read(cr, uid, action_id, context=context)

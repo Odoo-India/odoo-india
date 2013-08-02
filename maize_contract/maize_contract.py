fb5cecd491286dd0dce8f94dc3069031dc2bc283
Jignesh Rathod (OpenERP) <jir@tinyerp.com>
2013-08-02 17:14:52 +0530

    [Imp] Improve report for contract and add extend date.

--- maize_contract/maize_contract.py
+++ maize_contract/maize_contract.py
@@ -109,7 +109,7 @@ class purchase_order(osv.Model):
         diff_day = timedelta.days + float(timedelta.seconds) / 86400
         return diff_day
 
-    def onchange_compute_days1(self, cr, uid, ids, date_from, date_to,d1=0,d2=0):
+    def onchange_compute_days1(self, cr, uid, ids, date_from, date_to,d1=0,d2=0,d3=0):
         """
         If there are no date set for date_to, automatically set one 8 hours later than
         the date_from.
@@ -124,19 +124,19 @@ class purchase_order(osv.Model):
         # No date_to set so far: automatically compute one 8 hours later
         if date_from and not date_to:
             result['value']['no_of_days1'] = 0
-            result['value']['total_days'] = d1+d2
+            result['value']['total_days'] = d1+d2+d3
 
         # Compute and update the number of days
         if (date_to and date_from) and (date_from <= date_to):
             diff_day = self._get_number_of_days(date_from, date_to)
             result['value']['no_of_days1'] = round(math.floor(diff_day))+1
-            result['value']['total_days'] = d1+d2+round(math.floor(diff_day))+1
+            result['value']['total_days'] = d1+d2+round(math.floor(diff_day))+1+d3
         else:
             result['value']['no_of_days1'] = 0
 
         return result
     
-    def onchange_compute_days2(self, cr, uid, ids, date_from, date_to,d1=0,d2=0):
+    def onchange_compute_days2(self, cr, uid, ids, date_from, date_to,d1=0,d2=0,d3=0):
         """
         If there are no date set for date_to, automatically set one 8 hours later than
         the date_from.
@@ -151,19 +151,19 @@ class purchase_order(osv.Model):
         # No date_to set so far: automatically compute one 8 hours later
         if date_from and not date_to:
             result['value']['no_of_days2'] = 0
-            result['value']['total_days'] = d1+d2
+            result['value']['total_days'] = d1+d2+d3
 
         # Compute and update the number of days
         if (date_to and date_from) and (date_from <= date_to):
             diff_day = self._get_number_of_days(date_from, date_to)
             result['value']['no_of_days2'] = round(math.floor(diff_day))+1
-            result['value']['total_days'] = round(math.floor(diff_day))+1+d1+d2
+            result['value']['total_days'] = round(math.floor(diff_day))+1+d1+d2+d3
         else:
             result['value']['no_of_days2'] = 0
 
         return result
 
-    def onchange_compute_days3(self, cr, uid, ids, date_from, date_to,d1=0,d2=0):
+    def onchange_compute_days3(self, cr, uid, ids, date_from, date_to,d1=0,d2=0,d3=0):
         """
         If there are no date set for date_to, automatically set one 8 hours later than
         the date_from.
@@ -178,31 +178,61 @@ class purchase_order(osv.Model):
         # No date_to set so far: automatically compute one 8 hours later
         if date_from and not date_to:
             result['value']['no_of_days3'] = 0
-            result['value']['total_days'] = d1+d2
+            result['value']['total_days'] = d1+d2+d3
 
         # Compute and update the number of days
         if (date_to and date_from) and (date_from <= date_to):
             diff_day = self._get_number_of_days(date_from, date_to)
             result['value']['no_of_days3'] = round(math.floor(diff_day))+1
-            result['value']['total_days'] = round(math.floor(diff_day))+1+d1+d2
+            result['value']['total_days'] = round(math.floor(diff_day))+1+d1+d2+d3
         else:
             result['value']['no_of_days3'] = 0
 
         return result       
+
+    def onchange_compute_days4(self, cr, uid, ids, date_from, date_to,d1=0,d2=0,d3=0):
+        """
+        If there are no date set for date_to, automatically set one 8 hours later than
+        the date_from.
+        Also update the number_of_days.
+        """
+        # date_to has to be greater than date_from
+        if (date_from and date_to) and (date_from > date_to):
+            raise osv.except_osv(_('Warning!'),_('The start date must be anterior to the end date.'))
+
+        result = {'value': {}}
+
+        # No date_to set so far: automatically compute one 8 hours later
+        if date_from and not date_to:
+            result['value']['no_of_days4'] = 0
+            result['value']['total_days'] = d1+d2+d3
+
+        # Compute and update the number of days
+        if (date_to and date_from) and (date_from <= date_to):
+            diff_day = self._get_number_of_days(date_from, date_to)
+            result['value']['no_of_days4'] = round(math.floor(diff_day))+1
+            result['value']['total_days'] = round(math.floor(diff_day))+1+d1+d2+d3
+        else:
+            result['value']['no_of_days4'] = 0
+
+        return result
     
     _columns = {
         'contract': fields.related('indent_id', 'contract', type='boolean', relation='indent.indent', string='Contract', store=True, readonly=True),
         'no_of_days1': fields.integer("No of Days1", help="Calculate number of days for contracts"),
         'no_of_days2': fields.integer("No of Days2", help="Calculate Extended number of days 1st time for contracts"),
         'no_of_days3': fields.integer("No of Days3", help="Calculate Extended number of days 2nd time for contracts"),
+        'no_of_days4': fields.integer("No of Days4", help="Calculate Extended number of days 2nd time for contracts"),
         'total_days': fields.integer("Total Days", help="Calculate Total number of days for contracts"),
         'date_from': fields.date('From Date', required=True),
         'date_to': fields.date('To Date'),
         'extended_date_from1': fields.date('Extended From'),
         'extended_date_from2': fields.date('Extended From'),
+        'extended_date_from3': fields.date('Extended From'),
         'extended_date_to1': fields.date('Extended Upto'),
         'extended_date_to2': fields.date('Extended Upto'),
-        'retention': fields.selection([('leived', 'CONTRACTOR\'S RETENTION TO BE LEVIED'),('not_leived', 'CONTRACTOR\'S RETENTION NOT TO BE LEVIED')], 'Retention Type'),
+        'extended_date_to3': fields.date('Extended Upto'),
+        'retention': fields.selection([('leived', 'CONTRACTOR\'S RETENTION TO BE LEIVED'),('not_leived', 'CONTRACTOR\'S RETENTION NOT TO BE LEIVED')], "Retention Type"),
         }
     
     _defaults = {

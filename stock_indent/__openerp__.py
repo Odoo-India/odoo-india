ab94d1efdcb30c6f27a846d54e25e43119cd9efb
Rucha (Open ERP) <rpa@tinyerp.com>
2013-12-11 18:20:53 +0530

    [ADD]: stock_indent: Added html module description and updated rst description

--- stock_indent/__openerp__.py
+++ stock_indent/__openerp__.py
@@ -29,17 +29,29 @@
     'summary' : 'Manage and track internal material requests',
     'description' : """
 Indent Management
-================================================================================================================================================================ 
-Usually big companies setup and maintain internal requisition to be raised by Engineer, Plant Managers or Authorised Employees. 
+===================
+Usually big companies set-up and maintain internal requisition to be raised by Engineer, Plant Managers or Authorised Employees. Using Indent Management you can control the purchase and issue of material to employees within company warehouse.
 
-Using Indent Management you can control the purchase and issue of material to employees with in company warehouse.
-
-- Indents
+- Purchase Indents
 - Store purchase
 - Capital Purchase
+- Repairing Indents
 - Project Costing
 - Valuation
-- Etc
+- etc.
+
+Purchase Indents
+++++++++++++++++++
+When there is a need of specific materials or services, authorized employees or managers will create a Purchase Indent. He can specify required date, whether the indent is for store or capital, the urgency of materials etc. on indent.
+
+While selecting the product, the system will automatically set the procure method based on the quantity on hand for the product. Once the indent is approved, an internal move has been generated. A purchase order will also be generated if the products are not in stock and to be purchased.
+
+
+Repairing Indents
+++++++++++++++++++
+A store manager or will create a repairing indent when the product is needed to be sent for repairing. In case of repairing indent you will be able to select product to be repaired and service for repairing of the product.
+
+A purchase order is generated for the service taken for the supplier who repairs the product, and an internal move has been created for the product to be moved for repairing.
 """,
     'depends' : ['stock', 'purchase', 'product_container'],
     'data' : [

4688a8fe93c4630b3ad07f2d7c12e4a47a497b33
Rucha (Open ERP) <rpa@tinyerp.com>
2013-12-16 16:13:28 +0530

    [IMP]: product_container_tracking: Improved rst module description for new functionality

--- product_container_tracking/__openerp__.py
+++ product_container_tracking/__openerp__.py
@@ -42,10 +42,20 @@ When a product is configured with a container, the product is received or delive
 
 How it works?
 ++++++++++++++
+Container for single product
+------------------------------
 * Define product as container (e.g. Cylinder)
 * Select container in products (e.g. Oxygen Gas)
 * Receive incoming shipment of Oxygen Gas and Assign serial numbers (e.g. 001) for Oxygen Gas while processing it
 * OpenERP will automatically create an Incoming move for Cylinder with serial number (i.e. 001)
+
+Container for multiple product
+----------------------------------
+* Define product as container (e.g. Box 30x40x60)
+* Select container in packaging for product Mouse
+* Select 15 quantity of products and packaging in product sales tab
+* Receive incoming shipment of 15 Mouse and Assign serial numbers a0001 for Mouse and select pack 00000017 to indicate that the products are received in the same pack.
+* OpenERP will automatically create a serial number 00000017 for 1 qty of Box 30x40x60 and will assign 00000017 as a container serial number for a0001
 """,
     'depends' : ['product_container', 'stock_serial_tracking'],
     'data' : [

47fe4a39668bc724f2e292dfbf0c03aff2225369
Mantavya Gajjar <mga@tinyerp.com>
2013-12-19 18:09:45 +0530

    [IMP]: improve code to prepare an invoice, based on or receipt

--- l10n_in_purchase/l10n_in_purchase.py
+++ l10n_in_purchase/l10n_in_purchase.py
@@ -35,7 +35,8 @@ class purchase_order(osv.Model):
         :return: Value for fields of invoice lines.
         :rtype: dict
         """
-        return {
+        
+        res = {
             'name': order_line.name,
             'account_id': account_id,
             'price_unit': order_line.price_unit or 0.0,
@@ -46,6 +47,73 @@ class purchase_order(osv.Model):
             'account_analytic_id': order_line.account_analytic_id.id or False,
             'discount':order_line.discount or 0.0,
         }
+        return res
+    
+    def action_invoice_create(self, cr, uid, ids, context=None):
+        """Generates invoice for given ids of purchase orders and links that invoice ID to purchase order.
+        :param ids: list of ids of purchase orders.
+        :return: ID of created invoice.
+        :rtype: int
+        """
+        if context is None:
+            context = {}
+        journal_obj = self.pool.get('account.journal')
+        inv_obj = self.pool.get('account.invoice')
+        inv_line_obj = self.pool.get('account.invoice.line')
+
+        res = False
+        uid_company_id = self.pool.get('res.users').browse(cr, uid, uid, context=context).company_id.id
+        for order in self.browse(cr, uid, ids, context=context):
+            context.pop('force_company', None)
+            if order.company_id.id != uid_company_id:
+                #if the company of the document is different than the current user company, force the company in the context
+                #then re-do a browse to read the property fields for the good company.
+                context['force_company'] = order.company_id.id
+                order = self.browse(cr, uid, order.id, context=context)
+            pay_acc_id = order.partner_id.property_account_payable.id
+            journal_ids = journal_obj.search(cr, uid, [('type', '=', 'purchase'), ('company_id', '=', order.company_id.id)], limit=1)
+            if not journal_ids:
+                raise osv.except_osv(_('Error!'),
+                    _('Define purchase journal for this company: "%s" (id:%d).') % (order.company_id.name, order.company_id.id))
+
+            # generate invoice line correspond to PO line and link that to created invoice (inv_id) and PO line
+            inv_lines = []
+            for po_line in order.order_line:
+                acc_id = self._choose_account_from_po_line(cr, uid, po_line, context=context)
+                inv_line_data = self._prepare_inv_line(cr, uid, acc_id, po_line, context=context)
+                inv_line_id = inv_line_obj.create(cr, uid, inv_line_data, context=context)
+                inv_lines.append(inv_line_id)
+
+                po_line.write({'invoiced': True, 'invoice_lines': [(4, inv_line_id)]}, context=context)
+
+            # get invoice data and create invoice
+            inv_data = {
+                'name': order.partner_ref or order.name,
+                'reference': order.partner_ref or order.name,
+                'account_id': pay_acc_id,
+                'type': 'in_invoice',
+                'partner_id': order.partner_id.id,
+                'currency_id': order.pricelist_id.currency_id.id,
+                'journal_id': len(journal_ids) and journal_ids[0] or False,
+                'invoice_line': [(6, 0, inv_lines)],
+                'origin': order.name,
+                'fiscal_position': order.fiscal_position.id or False,
+                'payment_term': order.payment_term_id.id or False,
+                'company_id': order.company_id.id,
+                'package_and_forwording':order.amount_package_and_forwording,
+                'freight':order.amount_freight,
+                'insurance':order.amount_insurance
+            }
+            inv_id = inv_obj.create(cr, uid, inv_data, context=context)
+
+            # compute the invoice
+            inv_obj.button_compute(cr, uid, [inv_id], context=context, set_total=True)
+
+            # Link this new invoice to related purchase order
+            order.write({'invoice_ids': [(4, inv_id)]}, context=context)
+            res = inv_id
+        return res
+    
     def _get_order(self, cr, uid, ids, context=None):
         result = {}
         for line in self.pool.get('purchase.order.line').browse(cr, uid, ids, context=context):
@@ -125,7 +193,7 @@ class purchase_order(osv.Model):
             if order.insurance_type == 'include' and order.insurance:
                 included_price += order.insurance
                 order_total -= order.insurance
-                    
+            
             #Add fixed amount to order untax_amount
             if order.package_and_forwording_type in ('fix', 'include') and order.package_and_forwording:
                 other_charges += order.package_and_forwording
@@ -143,7 +211,7 @@ class purchase_order(osv.Model):
             res[order.id]['amount_tax'] = tax_total
             res[order.id]['amount_untaxed'] = untax_amount
             res[order.id]['other_charges'] = other_charges
-            res[order.id]['amount_total'] = round(order_total, 0)
+            res[order.id]['amount_total'] = order_total
             
         return res
     
@@ -176,6 +244,10 @@ class purchase_order(osv.Model):
                 'purchase.order': (lambda self, cr, uid, ids, c={}: ids, ['round_off','insurance', 'insurance_type', 'freight_type', 'freight', 'package_and_forwording_type', 'package_and_forwording', 'order_line'], 11),
                 'purchase.order.line': (_get_order, None, 10),
             }, multi="sums", help="Computed as Packing & Forwarding + Freight + Insurance"),
+     
+        'amount_package_and_forwording': fields.float('Packaging & Forwarding', states={'confirmed':[('readonly', True)], 'approved':[('readonly', True)], 'done':[('readonly', True)]}),
+        'amount_insurance': fields.float('Insurance', states={'confirmed':[('readonly', True)], 'approved':[('readonly', True)], 'done':[('readonly', True)]}),
+        'amount_freight': fields.float('Freight', states={'confirmed':[('readonly', True)], 'approved':[('readonly', True)], 'done':[('readonly', True)]}),
     }
     
     _defaults = {
@@ -194,27 +266,83 @@ class purchase_order(osv.Model):
             res.update({'package_and_forwording': 0.0})
         return {'value': res}
     
-purchase_order()
+    def wkf_confirm_order(self, cr, uid, ids, context=None):
+        res = super(purchase_order,self).wkf_confirm_order(cr, uid, ids, context)
+#         
+#         def _get_per_unit(order, line, amount, type):
+#             pass
+#         
+        for order in self.browse(cr, uid, ids, context):
+            order_line_pool = self.pool.get('purchase.order.line')
+            res_vals = {
+                'package_and_forwording':0.0,
+                'insurance':0.0,
+                'freight':0.0
+            }
+            total_pandf = 0.0
+            total_insurance = 0.0
+            total_freight = 0.0
+            
+            if order.amount_untaxed > 0:
+                #Add fixed amount to order percentage
+                if order.package_and_forwording_type == 'percentage' and order.package_and_forwording:
+                    total_pandf = order.amount_untaxed * (order.package_and_forwording / 100)
+                
+                if order.freight_type == 'percentage' and order.freight:
+                    total_freight = order.amount_untaxed * (order.freight / 100)
+                    
+                if order.insurance_type == 'percentage' and order.insurance:
+                    total_insurance = order.amount_untaxed * (order.insurance/100)
+            
+            #Add fixed amount to order untax_amount
+            if order.package_and_forwording_type in ('fix', 'include') and order.package_and_forwording:
+                total_pandf = order.package_and_forwording
+            
+            if order.freight_type in ('fix', 'include') and order.freight:
+                total_freight = order.freight
+                
+            if order.insurance_type in ('fix', 'include') and order.insurance:
+                total_insurance = order.insurance
+            
+            line_ids = []
+            for line in order.order_line:
+                line_rario = round((line.price_subtotal * 100) / order.amount_untaxed,2)
+                
+                if order.package_and_forwording_type == 'per_unit' and order.package_and_forwording:
+                    res_vals.update({'package_and_forwording':order.package_and_forwording})
+                    total_pandf = order.package_and_forwording * line.product_qty
+                elif not order.package_and_forwording_type == 'per_unit' and total_pandf:
+                    per_line = total_pandf * (line_rario / 100)
+                    per_unit = per_line / line.product_qty
+                    res_vals.update({'package_and_forwording':per_unit})
+                
+                if order.freight_type == 'per_unit' and order.freight:
+                    res_vals.update({'freight':order.freight})
+                    total_freight = order.freight * line.product_qty
+                elif not order.freight_type == 'per_unit' and total_freight:
+                    per_line = total_freight * (line_rario / 100)
+                    per_unit = per_line / line.product_qty
+                    res_vals.update({'freight':per_unit})
+                
+                if order.insurance_type in ('fix', 'percentage', 'include') and total_insurance:
+                    per_line = total_insurance * (line_rario / 100)
+                    per_unit = per_line / line.product_qty
+                    res_vals.update({'insurance':per_unit})
+                
+                line_ids.append(line.id)
+            
+            order_line_pool.write(cr, uid, line_ids, res_vals)
+            
+            order_vals = {
+                'amount_package_and_forwording':total_pandf,
+                'amount_insurance':total_freight,
+                'amount_freight':total_insurance
+            }
+            self.write(cr, uid, [order.id], order_vals)
 
-
-class mill_delivery(osv.Model):
-    _name = 'mill.delivery'
-    _description = 'Mill Delivery'
-
-    _columns = {
-        'name': fields.char('Name', size=64, required=True, translate=True),
-        'code': fields.char('Code', size=32, required=True),
-        'active': fields.boolean('Active', help="If the active field is set to False, it will allow you to hide the mill delivery without removing it."),
-    }
+        return res
     
-    _defaults = {
-        'active': True,
-    }
-    _sql_constraints = [
-        ('code_uniq', 'unique (code)', 'The code of the mill delivery must be unique!')
-    ]
-
-mill_delivery()
+purchase_order()
 
 class purchase_order_line(osv.Model):
     _inherit = 'purchase.order.line'
@@ -238,8 +366,32 @@ class purchase_order_line(osv.Model):
         'insurance': fields.float('Insurance Unit'),
         'freight': fields.float('Freight Unit'),
     }
-
+    
+    _defaults = {
+        'package_and_forwording': 0.0,
+        'insurance': 0.0,
+        'freight': 0.0
+    }
 purchase_order_line()
+
+class mill_delivery(osv.Model):
+    _name = 'mill.delivery'
+    _description = 'Mill Delivery'
+
+    _columns = {
+        'name': fields.char('Name', size=64, required=True, translate=True),
+        'code': fields.char('Code', size=32, required=True),
+        'active': fields.boolean('Active', help="If the active field is set to False, it will allow you to hide the mill delivery without removing it."),
+    }
+    
+    _defaults = {
+        'active': True,
+    }
+    _sql_constraints = [
+        ('code_uniq', 'unique (code)', 'The code of the mill delivery must be unique!')
+    ]
+
+mill_delivery()
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 

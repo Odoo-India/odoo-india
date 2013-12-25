fe5f91024a5ed9e7c078964fc8f5a294e2cdfba9
Mantavya Gajjar <mga@tinyerp.com>
2013-12-25 21:15:47 +0530

    [IMP]: add more options to configuration menu

--- l10n_in_base/res_config.py
+++ l10n_in_base/res_config.py
@@ -69,7 +69,14 @@ class indian_base_configuration(osv.osv_memory):
         'module_sale_after_service': fields.boolean('Manage after sale service using service contracts for products',
             help = """Allows you to keeps track of internal material request.
             It installs the stock_indent module."""),
-
+        
+        'module_l10n_in_account_tax': fields.boolean('Manage categories on Tax to differentiate taxes as per Indian Taxonomy',
+            help = """Allows you to keeps track of internal material request.
+            It installs the stock_indent module."""),
+        'module_l10n_in_invoice_adjust': fields.boolean('Adjust payable and receivables with each other using vouchers',
+            help = """Allows you to keeps track of internal material request.
+            It installs the stock_indent module."""),
+                
         'group_cst_config':fields.boolean('Enable Central Sales Tax on Partners', implied_group='l10n_in_base.group_cst_config', help = """TODO"""),
         'group_excise_config':fields.boolean('Enable Excise Control Code on Partners', implied_group='l10n_in_base.group_excise_config', help = """TODO"""),
         'group_tin_config':fields.boolean('Enable Tax Identification Number on Partners', implied_group='l10n_in_base.group_tin_config', help = """TODO"""),

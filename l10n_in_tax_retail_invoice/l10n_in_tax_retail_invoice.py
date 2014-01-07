a3211dde1e51deec392c20522595dc8a73a8518a
DJ Patel <mdi@tinyerp.com>
2014-01-07 12:07:52 +0530

    [IMP] l10n_in_tax_retail_invoice : Added the function to compute total quantity.

--- l10n_in_tax_retail_invoice/l10n_in_tax_retail_invoice.py
+++ l10n_in_tax_retail_invoice/l10n_in_tax_retail_invoice.py
@@ -19,8 +19,34 @@
 #
 ##############################################################################
 
-import l10n_in_tax_retail_invoice
-import res_company
-import report
+from openerp.osv import fields, osv
+from openerp.tools.amount_to_text_en import amount_to_text
+
+class account_invoice(osv.Model):
+    _inherit = 'account.invoice'
+
+    def _get_qty_total(self, cr, uid, ids):
+        res = {}
+        qty = 0.0
+        for invoice in self.browse(cr, uid, ids):
+            for line in invoice.invoice_line:
+                qty += line.quantity
+            res[invoice.id] = qty
+        return res
+
+    def amount_to_text(self, amount, currency):
+        '''
+        The purpose of this function is to use payment amount change in word
+        @param amount: pass Total payment of amount
+        @param currency: pass which currency to pay
+        @return: return amount in word
+        @rtype : string
+        '''
+        amount_in_word = amount_to_text(amount)
+        if currency == 'INR':
+            amount_in_word = amount_in_word.replace("euro", "Rupees").replace("Cents", "Paise").replace("Cent", "Paise")
+        return amount_in_word
+
+account_invoice()
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

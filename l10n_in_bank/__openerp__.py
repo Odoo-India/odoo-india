91ae617af6d4173751ac0d454f9a455513274e40
Atul Patel (OpenERP) <atp@tinyerp.com>
2014-01-20 14:48:46 +0530

    [ADD]l10n_in_bank: Add new module which list out all bank details.

---
+++ l10n_in_bank/__openerp__.py
@@ -0,0 +1,15 @@
+{ 'name':'Indian Bank Details',
+   'version':'1.0',
+   'author':'OpenERP S.A',
+   'category': 'Localization',
+   'website':'www.openerp.com',
+   'summary':'Bank Details',
+   'descritpion': """
+   This Module allow us 
+   ===============================
+   To Enter all Indian bank details and Bank Account Type.
+   """,
+   'depends': ['base'],
+   'data': ['l10n_in_bank_data.xml', 'l10n_in_bank_type_data.xml'],
+   'installable':True,
+}

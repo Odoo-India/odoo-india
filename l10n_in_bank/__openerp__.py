677e4bb3a8164208891fda23bd08d1a8644c2509
Atul Patel (OpenERP) <atp@tinyerp.com>
2014-01-20 15:52:28 +0530

    [IMP]: Improve l10n_in_bank module description [REM]: Remove other data file for bank type. merge it into one file bank_data.xml

--- l10n_in_bank/__openerp__.py
+++ l10n_in_bank/__openerp__.py
@@ -1,15 +1,39 @@
-{ 'name':'Indian Bank Details',
-   'version':'1.0',
-   'author':'OpenERP S.A',
-   'category': 'Localization',
-   'website':'www.openerp.com',
-   'summary':'Bank Details',
-   'descritpion': """
-   This Module allow us 
-   ===============================
-   To Enter all Indian bank details and Bank Account Type.
-   """,
-   'depends': ['base'],
-   'data': ['l10n_in_bank_data.xml', 'l10n_in_bank_type_data.xml'],
-   'installable':True,
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2013 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Banks Details',
+    'version': '1.0',
+    'category': 'Indian Localization',
+    'summary':'Indian Banks list, Banks Account Type list',
+    'description': """
+Indian Bank Details
+===============================
+This module contains All Indian Nationalised Banks and Bank Account type data. 
+""",
+    'author': 'OpenERP SA',
+    'website': 'http://www.openerp.com',
+    'images': [],
+    'depends': ['base'],
+    'data': ['l10n_in_bank_data.xml'],
+    'installable': True,
 }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
